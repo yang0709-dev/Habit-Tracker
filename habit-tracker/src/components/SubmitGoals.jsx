e841ea3d50f35252406a351ff71686a7ccf5dbf3
Scorp9514 <ertyu8571@gmail.com>
2025-11-11 22:14:00 +0800

    finished some basic styling on the page

---
+++ habit-tracker/src/components/SubmitGoals.jsx
@@ -0,0 +1,7 @@
+import "../css/submitgoals.css"
+
+function SubmitGoals(){
+    return <div id="submit-container">jdfaimfdioamoamfhgfghfhfhfhoidaf</div>
+}
+
+export default SubmitGoals
--- habit-tracker/src/pages/MainPage.jsx
+++ habit-tracker/src/pages/MainPage.jsx
@@ -1,18 +1,22 @@
 import "../css/mainpage.css"
+import SubmitGoals from "../components/SubmitGoals"
 
 function MainPage(){
     return <div className="mainContainer">
+        <div id="tilte">Habit Tracker</div>
         <div className="taskContainer">
             <div className="taskWrapper">
                 <input type="checkbox"></input>code
-                <div>fafadmfuiadsfnasduifnisadofndsanfdsnf</div>
+                <div className="taskDesc">goated project that helps you navigate through websites very </div>
             </div>
             <div className="taskWrapper">
                 <input type="checkbox"></input>run
-                <div>fudsnmvucxnvjuunmjbnuihbguidbjj</div>
+                <div className="taskDesc">fudsnmvucxnvjuunmjbnuihbguidbjj</div>
             </div>
             
         </div>
+        <hr className="hr-lines"/>
+        {<SubmitGoals/>}
     </div>
 }
 
