60d56b0cce71d9101b343e2000af71352b22bf4e
Scorp9514 <ertyu8571@gmail.com>
2025-11-15 20:44:27 +0800

    key saving is working now

--- habit-tracker/src/components/SubmitGoals.jsx
+++ habit-tracker/src/components/SubmitGoals.jsx
@@ -2,18 +2,31 @@ import "../css/submitgoals.css";
 import { useState, useContext } from "react";
 import { createContext } from "react";
 export const taskContext = createContext(["", ""]);
-import { get_Keys,set_Keys } from "../db/keysDB";
+
 import { set_Tasks } from "../db/tasksDB";
 
 function SubmitGoals() {
   const [tasks, setTasks] = useState(["", ""]);
 
+  if(localStorage.getItem('key') === null){
+    localStorage.setItem('key', 0);
+  }
+
+  function set_Keys(val){
+    localStorage.setItem('key',val)
+  }
+
+  function get_Keys(){
+    return localStorage.getItem('key')
+  }
 
   async function handleSubmit(){
     // assigning new key to the keysDB
-    let tasks_key = await get_Keys()
+
+
+    let tasks_key = Number(get_Keys())
     tasks_key+=1
-    await set_Keys(tasks_key);
+    set_Keys(tasks_key)
 
     // save the [name,desc] to the tasksDB
     await set_Tasks(tasks,tasks_key);
@@ -41,7 +54,7 @@ function SubmitGoals() {
             onChange={(e) => (tasks[1] = e.target.value)}
           ></input>
         </div>
-        <button id="submit-button" onClick={handleSubmit()}>
+        <button id="submit-button" onClick={()=>handleSubmit()}>
           Submit
         </button>
     </div>
