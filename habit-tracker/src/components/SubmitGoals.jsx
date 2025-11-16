33f614911586842a03ef0d45f727837175d152d2
Scorp9514 <ertyu8571@gmail.com>
2025-11-16 11:07:44 +0800

    checking the values of the inputs now and ready to save tasks into localStorage using keys

--- habit-tracker/src/components/SubmitGoals.jsx
+++ habit-tracker/src/components/SubmitGoals.jsx
@@ -3,39 +3,43 @@ import { useState, useContext } from "react";
 import { createContext } from "react";
 export const taskContext = createContext(["", ""]);
 
-import { set_Tasks } from "../db/tasksDB";
-
 function SubmitGoals() {
   const [tasks, setTasks] = useState(["", ""]);
 
-  if(localStorage.getItem('key') === null){
-    localStorage.setItem('key', 0);
+  if (localStorage.getItem("key") === null) {
+    localStorage.setItem("key", 0);
   }
 
-  function set_Keys(val){
-    localStorage.setItem('key',val)
+  function set_Keys(val) {
+    localStorage.setItem("key", val);
   }
 
-  function get_Keys(){
-    return localStorage.getItem('key')
+  function get_Keys() {
+    return localStorage.getItem("key");
   }
 
-  async function handleSubmit(){
+  async function handleSubmit() {
     // assigning new key to the keysDB
+    const taskName = document.getElementById('taskName')
+    const taskDesc = document.getElementById('taskDesc')
+    if (taskName.value != '' && taskDesc.value != ''){
+      console.log(' fdsadfsdafsdfas')
+      let tasks_key = Number(get_Keys());
+      tasks_key += 1;
+      set_Keys(tasks_key);
+      taskName.value = ''
+      taskDesc.value = ''
 
-
-    let tasks_key = Number(get_Keys())
-    tasks_key+=1
-    set_Keys(tasks_key)
-
-    // save the [name,desc] to the tasksDB
-    await set_Tasks(tasks,tasks_key);
+      // save the [name,desc] to the tasksDB
+      await set_Tasks(tasks, tasks_key);
+    }
+    
   }
-  
 
   return (
     <div id="submit-container">
-        <div id="submit-goals-text">Submit Your Goals</div>
+      <div id="submit-goals-text">Submit Your Goals</div>
+      <form>
         <div className="tasks-inputs">
           Name:{" "}
           <input
@@ -54,9 +58,10 @@ function SubmitGoals() {
             onChange={(e) => (tasks[1] = e.target.value)}
           ></input>
         </div>
-        <button id="submit-button" onClick={()=>handleSubmit()}>
+        <button type="button" id="submit-button" onClick={(e) => {handleSubmit();}}>
           Submit
         </button>
+      </form>
     </div>
   );
 }
--- habit-tracker/src/pages/MainPage.jsx
+++ habit-tracker/src/pages/MainPage.jsx
@@ -4,18 +4,12 @@ import CompletionRates from "../components/CompletionRates";
 import { useState,useContext } from "react";
 import { taskContext } from "../components/SubmitGoals";
 import Task from "../components/Task";
-import { get_Tasks_Keys,get_Tasks } from "../db/tasksDB";
 
 function MainPage() {
-  const tasks = useContext({taskContext})
-  async function test(){
-    return (await get_Tasks_Keys)
-  }
 
-  
   return (
     <div className="mainContainer">
-      <div className="taskContainer" onClick={console.log(test())}>
+      <div className="taskContainer">
         {<Task name="hi" desc="saying hello"/>}
       </div>
       <hr className="hr-lines" />
