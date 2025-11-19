7ccaaa60e56545fac3fa715216bde3836102a403
Scorp9514 <ertyu8571@gmail.com>
2025-11-19 23:47:57 +0800

    some css on history page

--- habit-tracker/src/App.jsx
+++ habit-tracker/src/App.jsx
@@ -2,6 +2,7 @@ import {Routes,Route} from 'react-router-dom'
 import MainPage from './pages/MainPage'
 import Graphs from './pages/Graphs';
 import Navbar from './components/Navbar';
+import History from './pages/History'
 import "./css/App.css"
 
 function App(){
@@ -13,6 +14,7 @@ function App(){
       <Routes>
         <Route path='/' element={<MainPage/>}/>
         <Route path='/Graphs' element={<Graphs/>}/>
+        <Route path='/History' element={<History/>}/>
       </Routes>
     </main>
   );
---
+++ habit-tracker/src/pages/History.jsx
@@ -0,0 +1,25 @@
+import "../css/history.css";
+
+export default function History() {
+  return (
+    <div id="history-container">
+      <div id="history-wrapper">
+        <div className="history">
+            <div className="date">
+                Nov 19
+            </div>
+            <div className="rate">
+                25%
+            </div>
+            <div className="history-info">
+                <ul className="history-name">
+                    <li>code</li>
+                    <li>run</li>
+                </ul>
+                
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
