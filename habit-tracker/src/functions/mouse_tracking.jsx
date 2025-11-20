fa1f39aef9e04ee938d0c83cbd05fd190dd362ab
Scorp9514 <ertyu8571@gmail.com>
2025-11-20 23:46:34 +0800

    deleted unnecessary package files and wrote some styles for history page, mouse tracking is yet to be fixed

---
+++ habit-tracker/src/functions/mouse_tracking.jsx
@@ -0,0 +1,12 @@
+export function handleHover(e){
+    const history = e.currentTarget
+    const history_info = history.querySelector(".history-info")
+    // console.log(history_info)
+    document.body.onpointermove = event =>{
+        const {clientX,clientY} = event
+        history_info.animate({
+            left:`${clientX/2}px`,
+            top:`${clientY/2}px`
+        },{duration:1000,fill:"forwards"})
+    }
+}
--- habit-tracker/src/pages/History.jsx
+++ habit-tracker/src/pages/History.jsx
@@ -1,20 +1,37 @@
 import "../css/history.css";
+import { handleHover } from "../functions/mouse_tracking";
 
 export default function History() {
   return (
     <div id="history-container">
       <div id="history-wrapper">
-        <div className="history">
+        <div className="history" onMouseOver={(e) => handleHover(e)}>
             <div className="date">
                 Nov 19
             </div>
             <div className="rate">
                 25%
             </div>
-            <div className="history-info">
+            <div className="history-info" >
                 <ul className="history-name">
-                    <li>code</li>
-                    <li>run</li>
+                    <li className="name">code</li>
+                    <li className="name">run</li>
+                </ul>
+                
+            </div>
+        </div>
+
+        <div className="history" onMouseEnter={(e) => handleHover(e)}>
+            <div className="date">
+                Nov 20
+            </div>
+            <div className="rate">
+                50%
+            </div>
+            <div className="history-info" >
+                <ul className="history-name">
+                    <li className="name">walk</li>
+                    <li className="name">run</li>
                 </ul>
                 
             </div>
